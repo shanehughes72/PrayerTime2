4eedcdba8ee274b0184bb6ae5331604d34796c2b
Shane Hughes <shughes@student.yosemite.edu>
2015-04-10 06:35:40 -0700

    Initial Commit

---
+++ PrayerTime2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrayerTime2
+//
+//  Created by Worship Arts on 4/10/15.
+//  Copyright (c) 2015 Shane Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
